6fed74b333b7e525c7709d0a575d20c4f0c2b0e2
malchililj <malchililj@gmail.com>
2024-09-27 05:00:00 -0400

    update

--- src/app/mortgage-form/mortgage-form.component.ts
+++ src/app/mortgage-form/mortgage-form.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ResultsDisplayComponent } from '../results-display/results-display.component';
 import { MortgageService } from '../mortgage.service';
+import { ClickOutsideDirective } from './click-outside.directive';
 
 @Component({
   selector: 'app-mortgage-form',
   templateUrl: './mortgage-form.component.html',
   styleUrls: ['./mortgage-form.component.css'],
   standalone: true,
-  imports: [CommonModule, FormsModule, ResultsDisplayComponent]
+  imports: [CommonModule, FormsModule, ResultsDisplayComponent, ClickOutsideDirective]
 })
 export class MortgageFormComponent {
   loanAmount: number = 0;
@@ -18,31 +19,40 @@ export class MortgageFormComponent {
   propertyValue: number = 0;
   monthlyDebt: number = 0;
   monthlyIncome: number = 0;
-  purchasePrice:number = 0;
-  grossHousehold:number = 0;
+  purchasePrice: number = 0;
+  grossHousehold: number = 0;
 
   monthlyPayment: number = 0;
   ltv: number = 0;
   dti: number = 0;
-  
-  isLoanTermDropdownOpen: boolean = true;
+
+  isLoanTermDropdownOpen: boolean = false;
   loanTermOptions = [
     { value: 30, label: '30 years' },
     { value: 35, label: '35 years' },
     { value: 40, label: '40 years' }
   ];
 
-  constructor(private mortgageService: MortgageService) {}
+  constructor(private mortgageService: MortgageService) { }
+
+  @HostListener('document:keydown.escape', ['$event'])
+  handleEscapeKey(event: KeyboardEvent) {
+    this.isLoanTermDropdownOpen = false;
+  }
 
   toggleLoanTermDropdown() {
     this.isLoanTermDropdownOpen = !this.isLoanTermDropdownOpen;
+  }
+
+  onClickOutside() {
+    this.isLoanTermDropdownOpen = false;
   }
 
   selectLoanTerm(term: number) {
     this.loanTerm = term;
     this.isLoanTermDropdownOpen = false;
   }
-  
+
   calculateMortgage() {
     this.monthlyPayment = this.mortgageService.calculateMonthlyPayment(this.loanAmount, this.interestRate, this.loanTerm);
     this.ltv = this.mortgageService.calculateLTV(this.loanAmount, this.propertyValue);
