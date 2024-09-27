045ed20c9402e2a68b853f3475196b50cb6ed068
malchililj <malchililj@gmail.com>
2024-09-27 04:37:59 -0400

    update

--- src/app/mortgage-form/mortgage-form.component.ts
+++ src/app/mortgage-form/mortgage-form.component.ts
@@ -18,13 +18,31 @@ export class MortgageFormComponent {
   propertyValue: number = 0;
   monthlyDebt: number = 0;
   monthlyIncome: number = 0;
+  purchasePrice:number = 0;
+  grossHousehold:number = 0;
 
   monthlyPayment: number = 0;
   ltv: number = 0;
   dti: number = 0;
+  
+  isLoanTermDropdownOpen: boolean = true;
+  loanTermOptions = [
+    { value: 30, label: '30 years' },
+    { value: 35, label: '35 years' },
+    { value: 40, label: '40 years' }
+  ];
 
   constructor(private mortgageService: MortgageService) {}
 
+  toggleLoanTermDropdown() {
+    this.isLoanTermDropdownOpen = !this.isLoanTermDropdownOpen;
+  }
+
+  selectLoanTerm(term: number) {
+    this.loanTerm = term;
+    this.isLoanTermDropdownOpen = false;
+  }
+  
   calculateMortgage() {
     this.monthlyPayment = this.mortgageService.calculateMonthlyPayment(this.loanAmount, this.interestRate, this.loanTerm);
     this.ltv = this.mortgageService.calculateLTV(this.loanAmount, this.propertyValue);
