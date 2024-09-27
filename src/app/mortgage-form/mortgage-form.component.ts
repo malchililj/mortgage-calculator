55e21f52c6bcbe7d17b83aa8bf960d0270ae765f
malchililj <malchililj@gmail.com>
2024-09-27 10:09:10 -0400

    update

--- src/app/mortgage-form/mortgage-form.component.ts
+++ src/app/mortgage-form/mortgage-form.component.ts
@@ -28,9 +28,11 @@ export class MortgageFormComponent {
 
   isLoanTermDropdownOpen: boolean = false;
   loanTermOptions = [
-    { value: 30, label: '30 years' },
     { value: 35, label: '35 years' },
-    { value: 40, label: '40 years' }
+    { value: 30, label: '30 years' },
+    { value: 25, label: '25 years' },
+    { value: 20, label: '20 years' },
+    { value: 15, label: '15 years' }
   ];
 
   constructor(private mortgageService: MortgageService) { }
